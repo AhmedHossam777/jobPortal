f629995d619b54b81bd81d58ed9c7bd4d345e628
AhmedHossam777 <ahmedhossam9226@gmail.com>
2026-06-01 05:49:52 +0300

    i

--- src/main/java/com/ahmed/learning/jobportal/company/controller/CompanyController.java
+++ src/main/java/com/ahmed/learning/jobportal/company/controller/CompanyController.java
@@ -15,6 +15,6 @@ public class CompanyController {
 
 	@GetMapping(version = "2.0")
 	public ResponseEntity<String> getCompanies2() {
-		return ResponseEntity.ok("Company List2");
+		return ResponseEntity.ok("Company List2 updated");
 	}
 }
